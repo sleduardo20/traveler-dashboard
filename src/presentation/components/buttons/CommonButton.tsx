e3596bd8389a484a0cdeca3ff092dc8386312343
sleduardo20 <sl.eduardo20@gmail.com>
2023-07-03 08:45:21 -0300

    🏭 create CommonButton component

---
+++ src/presentation/components/buttons/CommonButton.tsx
@@ -0,0 +1,63 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import { styled } from '../../styles';
+
+interface CommonButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text?: string;
+  icon?: React.ReactNode;
+  size?: 'small' | 'normal' | 'large';
+  color?: 'primary' | 'secondary' | 'ternary';
+}
+
+export function CommonButton({
+  text,
+  icon,
+  size = 'normal',
+  color = 'primary',
+  ...props
+}: CommonButtonProps) {
+  return (
+    <ButtonContainer color={color} size={size} {...props}>
+      {text && <small>{text}</small>} {icon && icon}
+    </ButtonContainer>
+  );
+}
+
+const ButtonContainer = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '1rem',
+  border: 'none',
+
+  small: {
+    fontSize: '$sm',
+  },
+
+  '&:disabled': {
+    opacity: '0.6',
+    cursor: 'no-drop',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.96)',
+  },
+
+  variants: {
+    size: {
+      small: {},
+      normal: {},
+      large: {
+        width: '41.6rem',
+        height: '7.2rem',
+      },
+    },
+    color: {
+      primary: {
+        background: '$orange',
+        color: '$white',
+      },
+      secondary: {},
+      ternary: {},
+    },
+  },
+});
